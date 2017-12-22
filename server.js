33909363f526bdc3280e26dfcadc7d8f58891e86
andychow122 <andychow122@gmail.com>
2017-12-22 11:27:38 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+//=======================================================setup
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var PORT = process.env.PORT || 3005;
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+//=======================================================
+
+
+
+
+
+
+
+//listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
