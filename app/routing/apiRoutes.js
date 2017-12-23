29902153a9c53b4e10f701bbfb37e1dc2ce11a80
andychow122 <andychow122@gmail.com>
2017-12-22 16:57:12 -0800

    dropdown check

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+var. friendsArray = require("../data/friends");
+
+module.exports = function(app){
+
+	app.get("/api/friends", function(req, res){
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+
+	app.post("/api/tables", function(req, res) {
+
+	friendsArray.push(req.body);
+	res.json(true);
+		
+	  });
+
+};
