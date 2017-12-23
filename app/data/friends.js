b7f147de6341c1ce3016f94daaaeccf18bfb1698
andychow122 <andychow122@gmail.com>
2017-12-22 17:51:24 -0800

    api check

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,18 @@
 var friendsArray = [{
-	name: "",
-	image: "",
-	scores: []
+	name: "Jackie Chan",
+	image: "https://www.thefamouspeople.com/profiles/images/jackie-chan-4.jpg",
+	scores: [
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"4",
+		"3",
+		"2",
+		"1",
+		"5"
+	]
 }];
 
 //to store all user info
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,19 @@
 var path = require("path");
 
-var. friendsArray = require("../data/friends");
+var friendsArray = require("../data/friends");
 
 module.exports = function(app){
 
 	app.get("/api/friends", function(req, res){
-		res.sendFile(path.join(__dirname, "../public/survey.html"));
+		res.json(friendsArray);
 	});
 
 	app.post("/api/tables", function(req, res) {
 
 	friendsArray.push(req.body);
 	res.json(true);
+
+
 		
 	  });
 
